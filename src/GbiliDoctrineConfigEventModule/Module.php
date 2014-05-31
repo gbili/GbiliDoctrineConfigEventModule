cdb455605403257d3ac8556f24832eb6f9eb29ca
Guillermo Devi <g@gbili.com>
2014-06-01 00:40:11 +0200

    autoload removed from Module.php

--- src/GbiliDoctrineConfigEventModule/Module.php
+++ src/GbiliDoctrineConfigEventModule/Module.php
@@ -3,20 +3,6 @@ namespace GbiliDoctrineConfigEventModule;
 
 class Module
 {
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-
     public function onBootstrap($e)
     {
         $sm = $e->getApplication()->getServiceManager();
